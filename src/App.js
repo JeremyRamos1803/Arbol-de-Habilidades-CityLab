1dfff9c8df5b683fd54575b33f212878857d323f
JeremyRamos1803 <jeremy.ramos@usm.cl>
2024-05-06 13:00:13 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+import { SkillProvider, SkillTreeGroup, SkillTree } from "beautiful-skill-tree";
+import { motion, harmony, cataclysm } from "./data/data";
+import theme from "./data/theme";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <div className="Image__container">
+        <img className="Image" src={logo} alt="borderlands logo." />
+      </div>
+      <SkillProvider>
+        <SkillTreeGroup theme={theme}>
+          {() => {
+            return (
+              <React.Fragment>
+                <SkillTree title="Motion" treeId="motion" data={motion} />
+                <SkillTree title="Harmony" treeId="harmony" data={harmony} />
+                <SkillTree
+                  title="Cataclysm"
+                  treeId="cataclysm"
+                  data={cataclysm}
+                />
+              </React.Fragment>
+            );
+          }}
+        </SkillTreeGroup>
+      </SkillProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav>
+      <ul className="NavBar__list">
+        <li>
+          <a href="https://github.com/andrico1234"></a>
+        </li>
+        <li>
+          <a href="https://www.buymeacoffee.com/1tsB7E9fL"></a>
+        </li>
+        <li>
+          <a href="https://andri.co"></a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/andricokaroulla/"></a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
